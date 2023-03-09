441b1ddfe7d9dbcc6dcbf4f182aa90cf34b294ad
AKASHDEEPMAHATA <akashdeepmahata@gmail.com>
2023-03-10 03:48:22 +0530

    contact form connected to firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,3 +140,60 @@ sr.reveal(`.home__social`, {delay: 600})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+
+
+
+
+
+
+
+
+// contact form - firebase
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5GehVo99OEOrQlh_gVm8sRD-UCIZ8pU0",
+    authDomain: "kitab-website-contact-form.firebaseapp.com",
+    databaseURL: "https://kitab-website-contact-form-default-rtdb.firebaseio.com",
+    projectId: "kitab-website-contact-form",
+    storageBucket: "kitab-website-contact-form.appspot.com",
+    messagingSenderId: "445792361259",
+    appId: "1:445792361259:web:cf38dd602ed3533649c36e"
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  var messagesRef = firebase.database()
+  .ref('Contact form data');
+  
+  document.getElementById('contactForm')
+  .addEventListener('submit', submitForm);
+  
+  function submitForm(e) {
+  e.preventDefault();
+  
+  // Get values
+
+  var email = getInputVal('email');
+  var sub = getInputVal('subject');
+  var message = getInputVal("message");
+  
+  saveMessage(email, subject , message);
+  document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get form values
+  function getInputVal(id) {
+  return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(email, subject , message) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    email: email,
+    subject:subject,
+    message: message,
+  });
+  }
